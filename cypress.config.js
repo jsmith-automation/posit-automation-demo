7a6a3cd038fb82398a5a44f19d1db8ad0eb438da
jsmith-automation <jsmith.qa.automation@gmail.com>
2023-03-10 08:43:52 -0500

    added headed run to CI job

--- cypress.config.js
+++ cypress.config.js
@@ -7,11 +7,11 @@ module.exports = defineConfig({
      require('cypress-mochawesome-reporter/plugin')(on)
     },
     baseUrl:"https://posit.cloud/",
-    viewportHeight: 1920,
-    viewportWidth: 1080,
+    // viewportHeight: 1920,
+    // viewportWidth: 1080,
     watchForFileChanges: false,
-    requestTimeout: 25000,
-    defaultCommandTimeout: 10000,
+    requestTimeout: 35000,
+    defaultCommandTimeout: 25000,
 
     env:{
       positUserName: "",
